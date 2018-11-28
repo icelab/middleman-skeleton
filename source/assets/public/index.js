5ecba5de35e7101338c89a76930e59a527312f02
Andy McCray <andymccray@andromeda.lan>
2018-11-29 10:24:48 +1100

    Don’t require view loader.

--- source/assets/public/index.js
+++ source/assets/public/index.js
@@ -1,13 +1,3 @@
-// Dependencies
-import domready from "domready";
-import viewloader from "viewloader";
-
-// Components
-//import views from "./views";
-
-// Kick off
-domready(() => {
-  // Execute normal viewloader stuff
-  //var manager = viewloader(views);
-  //manager.callViews();
-});
+/**
+ * JS goes here
+ */
